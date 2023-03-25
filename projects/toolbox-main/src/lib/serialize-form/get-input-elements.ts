1e8f4fdd6855d11c97d282e19b9aa9179c85f4b4
Fasguy <9297451+Fasguy@users.noreply.github.com>
2023-03-25 11:09:14 +0100

    Presets with ignored fields
    * Adding "data-preset-ignore" to an element will now prevent it being serialized to presets.

--- projects/toolbox-main/src/lib/serialize-form/get-input-elements.ts
+++ projects/toolbox-main/src/lib/serialize-form/get-input-elements.ts
@@ -1,6 +1,6 @@
 export function getInputElements(element: HTMLElement) {
 	return Array.prototype.filter.call(
 		element.querySelectorAll("input,select,textarea"),
-		(el) => !(el.tagName.toLowerCase() === "input" && (el.type === "submit" || el.type === "reset"))
+		(el: HTMLInputElement) => !(el.tagName.toLowerCase() === "input" && (el.type === "submit" || el.type === "reset" || el.dataset["presetIgnore"] !== undefined))
 	);
 }
