e85b4b5d88556b4ce3f06c959fbef59d6e3237f2
Fasguy <9297451+Fasguy@users.noreply.github.com>
2023-06-11 14:31:29 +0200

    Angular 16 collision IDs
    * Angular 16 shows NG0912 for components that have equivalent metadata. This is "problematic" for the window components (intructions, FAQ, etc.) as they don't (and shouldn't) have a selector.
    
    I'll keep an eye on https://github.com/angular/angular/issues/50158

--- projects/toolbox-main/src/app/loot-table-randomizer/views/frequently-asked-questions/frequently-asked-questions.component.ts
+++ projects/toolbox-main/src/app/loot-table-randomizer/views/frequently-asked-questions/frequently-asked-questions.component.ts
@@ -4,7 +4,9 @@ import { IWindow } from "src/app/common/views/window/window.component";
 @Component({
 	templateUrl: "./frequently-asked-questions.component.html",
 	styleUrls: ["./frequently-asked-questions.component.scss"],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	// See https://github.com/angular/angular/issues/50158
+	host: { "collision-id": "lt" }
 })
 export class LootTableRandomizerFAQComponent implements IWindow {
 	public title: string = "Loot-Table Randomizer - Frequently Asked Questions";
