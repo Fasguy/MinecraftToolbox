24c3a89158e66b098e68bb6fc660406acc92402a
Fasguy <9297451+Fasguy@users.noreply.github.com>
2023-11-11 10:26:50 +0100

    Credits
    * Credits can now be sped-up/reversed by using the scroll wheel
    * Credits can now be paused by clicking in the credits window

--- projects/toolbox-main/src/app/common/views/windows/credits/credits.component.ts
+++ projects/toolbox-main/src/app/common/views/windows/credits/credits.component.ts
@@ -10,6 +10,8 @@ import { IWindow } from "../../window/window.component";
 export class CreditsComponent implements OnInit, AfterViewInit, IWindow {
 	@ViewChild("text")
 	private _text!: ElementRef<HTMLPreElement>;
+	private _textAnimation!: Animation;
+	private _parallaxAnimation!: Animation;
 
 	protected credits!: Credit[];
 
@@ -17,6 +19,7 @@ export class CreditsComponent implements OnInit, AfterViewInit, IWindow {
 
 	public constructor(
 		private _changeDetector: ChangeDetectorRef,
+		private _elementRef: ElementRef
 	) {
 	}
 
@@ -37,6 +40,42 @@ export class CreditsComponent implements OnInit, AfterViewInit, IWindow {
 		textContainer.style.animationDuration = `${textContainer.clientHeight / speed}s`;
 
 		document.documentElement.style.setProperty("--startPos", `${textContainer.parentElement?.clientHeight}px`);
+
+		this._textAnimation = textContainer.getAnimations()[0];
+		this._parallaxAnimation = this._elementRef.nativeElement.getAnimations()[0];
+	}
+
+	@HostListener("wheel", ["$event"])
+	protected onWheel(e: WheelEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		const movementDelta = e.deltaY * 10;
+		(<number>this._textAnimation.currentTime) = Math.max((<number>this._textAnimation.currentTime) + movementDelta, 0);
+		(<number>this._parallaxAnimation.currentTime) = Math.max((<number>this._parallaxAnimation.currentTime) + movementDelta, 0);
+	}
+
+	private _drag: boolean = false;
+
+	@HostListener("pointerdown")
+	private onPointerDown() {
+		this._drag = false;
+	}
+
+	@HostListener("pointermove")
+	private onPointerMove() {
+		this._drag = true;
+	}
+
+	@HostListener("pointerup")
+	private onPointerUp() {
+		if (this._drag) {
+			return;
+		}
+
+		const newPlaybackRate = Number(!this._textAnimation.playbackRate);
+		this._textAnimation.updatePlaybackRate(newPlaybackRate);
+		this._parallaxAnimation.updatePlaybackRate(newPlaybackRate);
 	}
 }
 
